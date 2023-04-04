5bebb2fef05911cf643898b252799f08db0d2a12
yaseen <yaseen.alkoja@bootcamp-upgrade.com>
2023-04-04 17:51:49 +0200

    UPDATES4.0

--- main/pokedex.js
+++ main/pokedex.js
@@ -1,4 +1,6 @@
 console.log("adios");
+
+//*
 async function fetchPokeDex(id) {
 const url = `https://pokeapi.co/api/v2/pokemon/${id}`
 const res = await fetch(url);
@@ -7,39 +9,39 @@ const res = await fetch(url);
     return data;
 }
 
-
-
-
+//*
 const pokedex$$=document.querySelector('h1')
-
 const pokemonPage$$ = document.querySelector(".container")
-
 console.log(pokemonPage$$);
 
+//*
 const paintPokemons = (mappedPokemons)=> {
-
     pokemonPage$$.innerHTML =''
     
 
     for (const pokemon of mappedPokemons) {
         
+
         const card$$ = document.createElement("div")
         card$$.classList.add("card")    //Card div
         card$$.setAttribute('data-aos','fade-down')                       
         pokemonPage$$.appendChild(card$$)
         
+
         const flipCardInner$$ =document.createElement("div")
         flipCardInner$$.classList.add("flipCardInner")
         card$$.appendChild(flipCardInner$$)
+
 
         const flipCardFront$$ =document.createElement("div")
         flipCardFront$$.classList.add("flipCardFront")
         flipCardInner$$.appendChild(flipCardFront$$)
 
-        
+
         const flipCardBack$$ =document.createElement("div")
         flipCardBack$$.classList.add("flipCardBack")
         flipCardInner$$.appendChild(flipCardBack$$)
+
 
         const pokemonNum$$ = document.createElement("h3")
         pokemonNum$$.classList.add("pokemon-number")
@@ -51,10 +53,12 @@ const paintPokemons = (mappedPokemons)=> {
         flipCardBack$$.appendChild(cardName$$)
         cardName$$.textContent= pokemon.name
 
+
         const img$$ = document.createElement("img")
         img$$.classList.add("card-image")
         flipCardFront$$.appendChild(img$$)
         img$$.setAttribute("src", pokemon.image)
+
 
         const abilities$$ = document.createElement("p")
         abilities$$.classList.add("card-subtitle")
@@ -66,7 +70,7 @@ const paintPokemons = (mappedPokemons)=> {
         type$$.classList.add("card-subtitle")
         flipCardBack$$.appendChild(type$$)
         type$$.textContent=pokemon.type
-
+//* Conditional to apply background images depending on the type of pokemon 
         if (pokemon.type.includes("fire")) {
             img$$.style.background = 'url("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2U3NWE2ZWQ3MjU2YzkyNjYwNTkzODcyZWIyZDFkNTY1MzMwYzU4NSZjdD1n/fiyQQLci4d1w6Njojo/giphy.gif")';
           } else if (pokemon.type.includes("grass") || pokemon.type.includes("bug") || pokemon.type.includes("ground")) {
@@ -78,74 +82,61 @@ const paintPokemons = (mappedPokemons)=> {
           } else {
              img$$.style.background = 'url(https://media.giphy.com/media/5HK4TiiBeLSZq/giphy.gif)';
           }
-          
-
-
     }
 }
 
+//* Loop to show only the requested number of pokemons using their id
 async function fetchPokemon(){
-    const totalPokemons = 150;
+    const requestedPokemons = 300;
     const pokemons = [];
-    for (let index = 1;index <= totalPokemons; index++) {
+    for (let index = 1;index <= requestedPokemons; index++) {
         const pokemon = await fetchPokeDex(index);
         pokemons.push(pokemon);
 }
 return pokemons;
 }
 
-
-const mapPokemons = (pokemonsWithoutMapping) => { // mapping from main API
+//*Mapping required data from the API
+const mapPokemons = (pokemonsWithoutMapping) => {
     return pokemonsWithoutMapping.map((pokemon)=>({
         name: pokemon.name,
-        // order: pokemon.order,
         image: pokemon.sprites.other["official-artwork"]["front_default"],
         abilities: pokemon.abilities.map((ability) => ability.ability.name),
         type: pokemon.types.map((type) => type.type.name).join(', '),
         pokemonNum: pokemon.id,
-
     }))
 }
 
-
-
-
-
-
-
-const drawInput = (mappedPokemons) => { // creamos la funcion 
+//*Creating the search bar for the input field
+const drawInput = (mappedPokemons) => { 
     const input$$ = document.querySelector('input');
-    // const searchCardText$$ = document.querySelector('#search-card-text');
-
-    // searchCardText$$.textContent = input$$.value ? `Buscando: ${input$$.value}` : 'Ingrese el nombre de un Pokémon para buscar';
-
-    
     input$$.addEventListener("input", () => {
-        // searchCardText$$.textContent = input$$.value ? `Buscando: ${input$$.value}` : 'Ingrese el nombre de un Pokémon para buscar';
-        searchPokemons(input$$.value,mappedPokemons);
+    searchPokemons(input$$.value,mappedPokemons);
     });
 };
 
-
+//*Creating the required filters for the input field
 const searchPokemons = (filter, pokemons) => {
     // console.log(pokemons);
     let filteredPokemon= pokemons.filter((pokemon)=>pokemon.name.toLowerCase().includes(filter.toLowerCase()))
-// console.log(filteredPokemon);
+    // console.log(filteredPokemon);
     paintPokemons(filteredPokemon)
-
 }
 
 
+
+//*
 async function init() {
     const pokemons = await fetchPokemon();
     const mappedPokemons = mapPokemons(pokemons);
     // console.log(pokemons);
     paintPokemons(mappedPokemons);
     drawInput(mappedPokemons);
-
+//* Styles linked from https://michalsnik.github.io/aos/
     AOS.init();
 }
 
+//*
   init();
 
 
---
+++ todolist/todo.js
@@ -0,0 +1,95 @@
+const div$$ = document.createElement('div');
+div$$.classList.add('task-list-container');
+document.body.appendChild(div$$);
+
+const btn$$ = document.createElement('button');
+btn$$.classList.add('task-list-btn');
+btn$$.innerHTML = '<span class="add-icon">+</span>';
+div$$.appendChild(btn$$);
+
+const input$$ = document.createElement('input');
+input$$.classList.add('task-list-bar');
+input$$.setAttribute('placeholder', 'Add a task and press Enter...');
+div$$.appendChild(input$$);
+
+const ul$$ = document.createElement('ul');
+ul$$.classList.add('task-list');
+div$$.appendChild(ul$$);
+
+const completed$$ = document.createElement('ul');
+completed$$.classList.add('task-list', 'completed');
+div$$.appendChild(completed$$);
+
+const clearBtn$$ = document.createElement('button');
+clearBtn$$.classList.add('clear-completed-btn');
+clearBtn$$.textContent = 'Clear Completed';
+div$$.appendChild(clearBtn$$);
+
+const clearAllBtn$$ = document.createElement('button');
+clearAllBtn$$.classList.add('clear-all-btn');
+clearAllBtn$$.textContent = 'Clear All';
+div$$.appendChild(clearAllBtn$$);
+
+btn$$.addEventListener('click', addTask);
+input$$.addEventListener('keydown', function(event) {
+  if (event.keyCode === 13) {
+    addTask();
+  }
+});
+
+function addTask() {
+  const taskText = input$$.value.trim();
+  if (taskText === '') {
+    // Don't add empty tasks
+    return;
+  }
+
+  const task$$ = document.createElement('li');
+  task$$.textContent = taskText;
+  task$$.classList.add('task-list-item');
+
+  const deleteBtn$$ = document.createElement('button');
+  deleteBtn$$.classList.add('delete-task-btn');
+  deleteBtn$$.innerHTML = '<span class="delete-icon">x</span>';
+  task$$.appendChild(deleteBtn$$);
+
+  const completeBtn$$ = document.createElement('button');
+  completeBtn$$.classList.add('complete-task-btn');
+  completeBtn$$.innerHTML = '<span class="complete-icon">✓</span>';
+  task$$.appendChild(completeBtn$$);
+
+  ul$$.appendChild(task$$);
+  input$$.value = ''; // clear the input field
+
+  // Add animation class to task element
+  setTimeout(() => {
+    task$$.classList.add('task-list-item--show');
+  }, 10);
+
+  deleteBtn$$.addEventListener('click', function() {
+    task$$.parentNode.removeChild(task$$);
+  });
+
+  completeBtn$$.addEventListener('click', function() {
+    task$$.classList.add('task-list-item--completed');
+    completed$$.appendChild(task$$);
+  });
+}
+
+clearBtn$$.addEventListener('click', function() {
+  const completedTasks$$ = completed$$.querySelectorAll('.task-list-item');
+  completedTasks$$.forEach(function(task$$) {
+    task$$.parentNode.removeChild(task$$);
+  });
+});
+
+clearAllBtn$$.addEventListener('click', function() {
+  const allTasks$$ = document.querySelectorAll('.task-list-item');
+  allTasks$$.forEach(function(task$$) {
+    task$$.parentNode.removeChild(task$$);
+  });
+  const completedTasks$$ = document.querySelectorAll('.completed .task-list-item');
+  completedTasks$$.forEach(function(task$$) {
+    task$$.parentNode.removeChild(task$$);
+  });
+});
